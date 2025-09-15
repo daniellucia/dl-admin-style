c2cd86e2c54dbdb3960c38a8a97a8790d8bca147
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-15 06:56:53 +0200

    Añadimos preconenct

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,13 +19,14 @@ class Plugin
     public function init(): void
     {
         add_action('admin_enqueue_scripts', array($this, 'enqueue_styles'), 100);
+        add_action('admin_head', array($this, 'add_admin_preconnect'), 5);
     }
 
     public function enqueue_styles()
     {
         wp_enqueue_style(
             'dl-admin-google-fonts',
-            'https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+            'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
             [],
             ver: $this->version
         );
@@ -36,5 +37,16 @@ class Plugin
             ['dl-admin-google-fonts'],
             $this->version
         );
+    }
+
+    /**
+     * Añadimos preconnect para las fuentes de Google
+     * @return void
+     * @author Daniel Lucia
+     */
+    public function add_admin_preconnect()
+    {
+        echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n";
+        echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n";
     }
 }
