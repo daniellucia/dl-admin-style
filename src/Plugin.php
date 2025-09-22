7210104f676caa472ffd73d8d4097a024f519545
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-22 07:34:31 +0200

    Fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -28,7 +28,7 @@ class Plugin
             'dl-admin-google-fonts',
             'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
             [],
-            ver: $this->version
+            $this->version
         );
 
         wp_enqueue_style(
